9da0c40e3a33462bb25957ed8bda06feebb721c6
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-02-25 17:37:15 +0300

    using a castom observable from scratch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 import 'rxjs/Rx';
 
 @Component({
@@ -12,11 +12,32 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
-      }
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 12000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 14000);
+      setTimeout(() => {
+        // observer.error('this does not work');
+        observer.complete();
+      }, 15000);
+      setTimeout(() => {
+        observer.next('third package');
+      }, 16000);
+    });
+    myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('completed'); }
     );
   }
 
